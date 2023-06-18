acf620d06fb06b14c4ca05e394596e21bac07c9b
Urothis <natecaple@gmail.com>
2023-06-17 21:01:04 -0500

    Wrestlin with version

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
+
+
 const config: DocsThemeConfig = {
   logo: <span><img src="https://raw.githubusercontent.com/nwn-rs/.github/main/profile/assets/logo/icon.svg" alt="Project Logo" width="24" height="24" /></span>,
   primaryHue: 29,
@@ -15,7 +17,19 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/nwn-rs/web/blob/main/',
   footer: {
-    text: 'nwn.rs' + (process.env.VERCEL_GIT_COMMIT_SHA ? '@' + process.env.VERCEL_GIT_COMMIT_SHA.toString().slice(0, 7) : ''),
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} {' '}
+        <a href="https://nextra.site" target="_blank">
+          nwn.rs
+          {process.env.VERCEL_GIT_COMMIT_SHA &&
+            `@${process.env.VERCEL_GIT_COMMIT_SHA.toString().slice(0, 7)}`
+          }
+        </a>
+      </span>
+    )
+
+    //text: 'nwn.rs' + (process.env.VERCEL_GIT_COMMIT_SHA ? '@' + process.env.VERCEL_GIT_COMMIT_SHA.toString().slice(0, 7) : ''),
   },
 }
 
