873dd1baa3adea773ad8b562b1d3fb423ce39e3e
Urothis <natecaple@gmail.com>
2023-06-17 20:40:41 -0500

    version

--- theme.config.tsx
+++ theme.config.tsx
@@ -15,7 +15,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/nwn-rs/web/blob/main/',
   footer: {
-    text: 'nwn.rs@' + process.env.VERCEL_GIT_COMMIT_SHA
+    text: 'nwn.rs' + (process.env.VERCEL_GIT_COMMIT_SHA ? '@' + process.env.VERCEL_GIT_COMMIT_SHA.toString().slice(0, 7) : ''),
   },
 }
 
